468507ed7a18694a631de6ed50bf8b907013fee4
Iago Calazans <iago.calazans@gmail.com>
2021-11-18 10:01:21 -0300

    fix(regex): avoid to capture while less 2 '_'
    changed the regex to `(\\x5f|_|\\_){2}proto(\\x5f|_|\\_){2}(?=["']+[\s:]+[{"'])` this way we avoid capturing `_proto_`
    
    #2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var noPollutionDefaultJsonParser = JSON.parse;
 var safeParser = function(jsonString, reviver) {
-  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\x5f|_|\\_){1,2}proto(\\x5f|_|\\_){1,2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
+  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\x5f|_|\\_){2}proto(\\x5f|_|\\_){2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
 };
 JSON.parse = safeParser;
