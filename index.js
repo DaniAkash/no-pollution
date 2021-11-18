5db0c1db3f618718471701493acaa91f966416e6
Iago Calazans <iago.calazans@gmail.com>
2021-11-18 09:34:35 -0300

    fix(replace): using regex to look for key only
    the regex `(\\u5f|_){1,2}proto(\\u5f|_){1,2}(?=["']+[\s:]+[{"'])` passed all tests at https://regex101.com/r/Q6ba6D/3
    
    #2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var noPollutionDefaultJsonParser = JSON.parse;
 var safeParser = function(jsonString, reviver) {
-  return noPollutionDefaultJsonParser(typeof jsonString === "string"? jsonString.replace(/"__proto__"/g, '"__pollutants__"'): jsonString, reviver);
+  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\u5f|_){1,2}proto(\\u5f|_){1,2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
 };
 JSON.parse = safeParser;
