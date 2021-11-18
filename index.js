7f10eacb4ef819c6265686f9d52f3493ffc4dcda
Iago Calazans <iago.calazans@gmail.com>
2021-11-18 10:08:02 -0300

    fix(regex): looking for escape with uppercase too
    final regex result `(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}proto(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}(?=["']+[\s:]+[{"'])` ready to go
    
    #2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var noPollutionDefaultJsonParser = JSON.parse;
 var safeParser = function(jsonString, reviver) {
-  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\x5f|_|\\_){2}proto(\\x5f|_|\\_){2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
+  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}proto(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
 };
 JSON.parse = safeParser;
