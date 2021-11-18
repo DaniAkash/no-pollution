f23750947d515737837bd80c6b164054abb8c8dc
Iago Calazans <iago.calazans@gmail.com>
2021-11-18 09:51:58 -0300

    fix(regex): change the escape capture range
    the escape test was wrong cus the value is `\x5f`
    
    #2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var noPollutionDefaultJsonParser = JSON.parse;
 var safeParser = function(jsonString, reviver) {
-  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\u5f|_){1,2}proto(\\u5f|_){1,2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
+  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\x5f|_|\\_){1,2}proto(\\x5f|_|\\_){1,2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
 };
 JSON.parse = safeParser;
