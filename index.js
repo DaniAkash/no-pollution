25671d06aac6b3b46c929994b0c81e2daad9d20a
Iago Calazans <iago.calazans@gmail.com>
2021-11-18 11:59:37 -0300

    refactor(regex): shortening the regex expression
    the regex expression was so much bigger, so i've made a shorten version: `/(_|\\_|[\\u05fFx]+){2}proto(_|\\_|[\\u05fFx]+){2}(?=["']+[\s:]+[{"'])/g`.
    
    #2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var noPollutionDefaultJsonParser = JSON.parse;
 var safeParser = function(jsonString, reviver) {
-  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}proto(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
+  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(_|\\_|[\\u05fFx]+){2}proto(_|\\_|[\\u05fFx]+){2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
 };
 JSON.parse = safeParser;
