3bfe3f419d49acd1ab157c7b9655161c4942fedd
Iago Calazans <iago.calazans@gmail.com>
2021-11-26 07:06:27 -0300

    fix(replace): using regex to look for key only (#3)
    * fix(replace): using regex to look for key only
    
    the regex `(\\u5f|_){1,2}proto(\\u5f|_){1,2}(?=["']+[\s:]+[{"'])` passed all tests at https://regex101.com/r/Q6ba6D/3
    
    #2
    
    * fix(regex): change the escape capture range
    
    the escape test was wrong cus the value is `\x5f`
    
    #2
    
    * fix(regex): avoid to capture while less 2 '_'
    
    changed the regex to `(\\x5f|_|\\_){2}proto(\\x5f|_|\\_){2}(?=["']+[\s:]+[{"'])` this way we avoid capturing `_proto_`
    
    #2
    
    * fix(regex): looking for escape with uppercase too
    
    final regex result `(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}proto(\\x5f|\\x5F|_|\\_|\\u005f|\\u005F){2}(?=["']+[\s:]+[{"'])` ready to go
    
    #2
    
    * refactor(regex): shortening the regex expression
    
    the regex expression was so much bigger, so i've made a shorten version: `/(_|\\_|[\\u05fFx]+){2}proto(_|\\_|[\\u05fFx]+){2}(?=["']+[\s:]+[{"'])/g`.
    
    #2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var noPollutionDefaultJsonParser = JSON.parse;
 var safeParser = function(jsonString, reviver) {
-  return noPollutionDefaultJsonParser(typeof jsonString === "string"? jsonString.replace(/"__proto__"/g, '"__pollutants__"'): jsonString, reviver);
+  return noPollutionDefaultJsonParser(typeof jsonString === "string" ? jsonString.replace(/(_|\\_|[\\u05fFx]+){2}proto(_|\\_|[\\u05fFx]+){2}(?=["']+[\s:]+[{"'])/g, '__pollutants__') : jsonString, reviver);
 };
 JSON.parse = safeParser;
